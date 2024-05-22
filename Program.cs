e8ae5a31d43efa149fe4c0556a97a9fc9d3af3b2
ymd65536 <ymd65536@gmail.com>
2024-05-23 00:15:19 +0900

    fix: set Environment Variable

--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
-﻿GetSecretVersionSample test = new();
+﻿string PROJECT_ID = Environment.GetEnvironmentVariable("PROJECT_ID")!;
+
+if (string.IsNullOrEmpty(PROJECT_ID))
+{
+    Console.WriteLine("PROJECT_ID environment variable must be set.");
+    return;
+}
+
+GetSecretVersionSample test = new();
 var result = test.GetSecretVersion(
-    projectId: "pj-id", secretId: "GOOGLE_CLIENT_SECRET", secretVersionId: "1"
+    projectId: PROJECT_ID, secretId: "GOOGLE_CLIENT_SECRET", secretVersionId: "1"
 );
 
 Console.WriteLine($"Secret version {result.Name} retrieved.");
 
 GetSecretSample test2 = new();
-var result2 = test2.GetSecret(projectId: "pj-id", secretId: "GOOGLE_CLIENT_SECRET");
+var result2 = test2.GetSecret(projectId: PROJECT_ID, secretId: "GOOGLE_CLIENT_SECRET");
 
 Console.WriteLine($"Secret {result2} retrieved.");
 
 
 AccessSecretVersionSample test3 = new();
-var result3 = test3.AccessSecretVersion(projectId: "pj-id", secretId: "GOOGLE_CLIENT_SECRET", secretVersionId: "1");
+var result3 = test3.AccessSecretVersion(projectId: PROJECT_ID, secretId: "GOOGLE_CLIENT_SECRET", secretVersionId: "1");
 
 Console.WriteLine($"Secret {result3} retrieved.");
--- README.md
+++ README.md
@@ -9,3 +9,15 @@ dotnet add package Google.Cloud.SecretManager.V1 --version 2.5.0
 ```
 
 [link](https://www.nuget.org/packages/Google.Cloud.SecretManager.V1)
+
+環境変数をセットする。
+
+```bash
+$env:PROJECT_ID="your-project"
+```
+
+または
+
+```bash
+export PROJECT_ID="your-project"
+```
